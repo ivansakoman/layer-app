4d01e78d612ff6444a68157cdbb045a5a4230176
ivan.sakoman <saka1993@hotmail.com>
2022-03-08 22:43:01 +0100

    ignore lint errors

--- src/app/utils/useOutsideAlerter.ts
+++ src/app/utils/useOutsideAlerter.ts
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react';
 
 //close filter dropdown on click outside of it
--- src/features/home-screen/components/SearchInput.tsx
+++ src/features/home-screen/components/SearchInput.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { debounce } from 'lodash';
 import { useCallback } from 'react';
 
